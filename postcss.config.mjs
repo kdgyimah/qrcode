103479a77d24ac2be479b7c856c8d3abe4fd61c6
Daniel Essel <dessel015@gmail.com>
2025-06-27 13:32:33 +0000

    feat: update styles and configuration for improved theming and compatibility with PostCSS plugins

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,7 @@
-const config = {
-    plugins: {
-      "@tailwindcss/postcss": {},
-    },
-  };
-  export default config;
+// postcss.config.mjs
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [tailwindcss, autoprefixer],
+};
